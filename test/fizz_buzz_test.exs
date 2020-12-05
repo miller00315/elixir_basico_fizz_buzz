db5a45c1adb720fe5536a37c4a9a1187212a6835
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-12-05 14:29:03 -0300

    Ajustando variaveis de test

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -3,13 +3,13 @@ defmodule FizzBuzzTest do
 
   describe "build/1" do
     test "when a valid file is provided, returns the converted list" do
-      assert FizzBuzz.build("numbers.txt") ==
-               {:ok, [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, :fizzBuzz]}
+      expectedResponse = {:ok, [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, :fizzBuzz]}
+      assert FizzBuzz.build("numbers.txt") == expectedResponse
     end
 
     test "when a invalid file is provided, returns a message with the error" do
-      assert FizzBuzz.build("invalid.txt") ==
-               {:error, "Erro read the file enoent"}
+      expectedResponse = {:error, "Erro read the file enoent"}
+      assert FizzBuzz.build("invalid.txt") == expectedResponse
     end
   end
 end
