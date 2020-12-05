5de16c8d9b2548d6f7a6ace1436dda4ca7a974d0
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-12-05 14:27:46 -0300

    Ajustando erros

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      assert FizzBuzz.build("numbers.txt") ==
+               {:ok, [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, :fizzBuzz]}
+    end
+
+    test "when a invalid file is provided, returns a message with the error" do
+      assert FizzBuzz.build("invalid.txt") ==
+               {:error, "Erro read the file enoent"}
+    end
   end
 end
